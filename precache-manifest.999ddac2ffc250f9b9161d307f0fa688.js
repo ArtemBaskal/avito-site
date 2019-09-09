ea89b18ee69618fd14a590ef8392333908b4976a
ArtemBaskal <asbaskal@edu.hse.ru>
2019-09-09 22:31:12 +0300

    Updates

---
+++ precache-manifest.999ddac2ffc250f9b9161d307f0fa688.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c934883a8ed33d5dc9314d5da5e77a",
+    "url": "/Avito-site/index.html"
+  },
+  {
+    "revision": "bb090123ac2aae3fe6ff",
+    "url": "/Avito-site/static/css/main.f59437ee.chunk.css"
+  },
+  {
+    "revision": "641ab32285b862d7a1e9",
+    "url": "/Avito-site/static/js/2.34047305.chunk.js"
+  },
+  {
+    "revision": "bb090123ac2aae3fe6ff",
+    "url": "/Avito-site/static/js/main.530a6005.chunk.js"
+  },
+  {
+    "revision": "a014397f63f49d8cfc3f",
+    "url": "/Avito-site/static/js/runtime~main.3446433e.js"
+  }
+]);
